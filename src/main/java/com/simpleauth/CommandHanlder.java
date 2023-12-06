6e6da32b68803b102b7f1af41f71e3f6220d33c6
Dainoso <hungmanh190204.github@gmail.com>
2023-12-06 11:59:43 +0700

    test

--- src/main/java/com/simpleauth/CommandHanlder.java
+++ src/main/java/com/simpleauth/CommandHanlder.java
@@ -4,31 +4,45 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import java.util.regex.*;
 
 public class CommandHanlder implements CommandExecutor{
+
+    public boolean getEmail(String email) {
+        String regex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(email);
+        boolean matchFound = matcher.find();
+
+        if (matchFound) {return true;}
+        else {return false;}
+    }
+
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
         Player player = (Player)sender;
         String email = args[0];
         String password = args[1];
-        String[] errorMessage = {"§cInvalid Input!", "§cPlease try again ..."};
-        String[] successfullyMessage = {"§aSuccessfully.", "Your email had been added!"};
+        
         if (command.getName().equalsIgnoreCase("register") && sender instanceof Player){
             player.sendMessage("§aTrying To Register Account...");
             Plugin.LOGGER.info("registerring player " + player.getName() + "with password " + password);
             return true;
         }
 
+        // add email to restore account if users have stolen the password
         if (command.getName().equalsIgnoreCase("addemail") && sender instanceof Player) {
                 
-    
-                if (password == null || email == null) {
-                    player.sendMessage(errorMessage);
-                    return false;
-                } else {
-                    player.sendMessage(successfullyMessage);
+                if (email != null) {
+                    player.sendMessage("§aSuccessfully.");
+                    player.sendMessage("Your email had been added!");
                     Plugin.LOGGER.info("Email player " + player.getName() + " has been added.");
                     return true;
+                } else {
+                    player.sendMessage("§cInvalid Input!");
+                    player.sendMessage("§cPlease try again ...");
+                    return false;
                 }
     
             }
